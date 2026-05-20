7be5abeaa00dbf59382195f19f9a41234d8e2cef
Juan <jspowerzone@gmail.com>
2026-05-19 19:35:16 -0500

    Por angela

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, browserLocalPersistence, setPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAAvNXjtQKO4mwN9rEEyk3kUHJ7Iz8hjVs",
   authDomain: "clases-gratis-app.firebaseapp.com",
   projectId: "clases-gratis-app",
-storageBucket: "clases-gratis-app.appspot.com",
+  storageBucket: "clases-gratis-app.appspot.com",
   messagingSenderId: "902148131039",
   appId: "1:902148131039:web:4715888e0155baf979c16a"
 };
@@ -14,5 +14,6 @@ storageBucket: "clases-gratis-app.appspot.com",
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence); // 👈 esto mantiene la sesión al cerrar la app
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
